6d7d46b8a08c24ec226a4dff8efc356c83376fad
João Pedro Venturini Sales <jpventurinisales@gmail.com>
2023-10-13 16:14:44 -0300

    Atualização 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { api } from './lib/axios'
-
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
 
   const [idPokemon,setIdPokemon] = useState (1)
   const [namePokemon,setNamePokemon] = useState ("")
   const [data,setData] = useState ([])
+
   const getPokemon = async() => {
     const response = await api(`/pokemon/${idPokemon}`)
     setData(response.data)
   }
-  const inputPokemon = async() => {
-    const response = await api(`/pokemon/${namePokemon}`)
-    setData(response.data)
-  }
 
-  useEffect(()=>{
-    getPokemon()
-    inputPokemon()
-  },[idPokemon, namePokemon])
+   const getPokemonByName = async () => {
+    setNamePokemon(namePokemon);
+  };
 
-  const somar = () => {
+  const buttonSearch = async () => {
+    const id = toast.loading("Buscando pokemon");
+    try {
+      const response = await api.get(`/pokemon/${namePokemon.toLowerCase()}`);
+      toast.update(id, {
+        render: "Pokemon encontrado",
+        type: "success",
+        isLoading: false,
+        autoClose: 2000,
+      });
+      setData(response.data);
+      setIdPokemon(response.data.id);
+      setNamePokemon('');
+    } catch (error) {
+      toast.update(id, {
+        render: "Pokemon não encontrado",
+        type: "warning",
+        isLoading: false,
+        autoClose: 2000,
+      });
+      setNamePokemon('');
+    }
+  };
+
+  useEffect(() => {
+      getPokemon();
+  }, [idPokemon]);
+
+  const proximo = () => {
+    if (idPokemon < 1010) {
     setIdPokemon(idPokemon+1)
+    }
   }
 
-  const subtrair = () => {
-    setIdPokemon(idPokemon-1)
-    if (idPokemon < 1) {
-      setIdPokemon(1)
+  const anterior = () => {
+    if (idPokemon > 1) {
+      setIdPokemon(idPokemon - 1);
     }
   }
 
@@ -43,7 +70,7 @@ const App = () => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
 
-      <img src={data.sprites.front_default} className='pokemon'/>
+      <img src={data?.sprites?.front_default} className='pokemon'/>
       
       <h1 className='data'>
         <span className="pokemonId">{data?.id}</span> - <span className="pokemonName">{data?.name}</span>
@@ -54,26 +81,20 @@ const App = () => {
       </form>
 
     <div className="button-clear">
-      <button className="button-circle btn-clear"></button>
+      <button className="button-circle btn-clear" onClick={buttonSearch}>Buscar</button>
     </div>
 
     <div className='button-left'>
-      <button className='button btn-left' onClick={subtrair}></button>
+      <button className='button btn-left' onClick={anterior}><FontAwesomeIcon icon={faArrowLeft} style={{ color: "white" }} /></button>
     </div>
 
     <div className='button-right'>
-      <button className='button btn-right' onClick={somar}></button>
-    </div>
-
-    <div className="button-up">
-      <button className='button btn-up'></button>
-    </div>
-
-    <div className="button-down">
-      <button className='button btn-down'></button>
+      <button className='button btn-right' onClick={proximo}><FontAwesomeIcon icon={faArrowRight} style={{ color: "white" }} /></button>
     </div>
 
       <img src="../src/assets/pokedex.png" alt='Pokedex' className='pokedex'/>
+
+      <ToastContainer/>
     </main>
   )
 }
